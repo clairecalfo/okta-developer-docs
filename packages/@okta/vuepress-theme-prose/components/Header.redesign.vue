c21cd0c8aa5a226a5bc28aa4e991927ac5e9c088
François Lehoux <francois.lehoux@okta.com>
2020-12-11 16:11:11 -0500

    Add first draft of a header component

---
+++ packages/@okta/vuepress-theme-prose/components/Header.redesign.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="redesign">
+    <a href="https://developer.okta.com/" class="header--logo">
+      <img src="/img/icons/okta-developer.svg" />
+    </a>
+    <input type="text" placeholder="Search Developer" class="temp-search" />
+    <div class="header--links">
+      <MenuItems
+        :items="$themeConfig.redesign.primary_left_nav"
+        :itemCss="'link link--small link--semi-bold'"
+        :subItemCss="'link link--small link--semi-bold link--black'"
+      />
+      <MenuItems
+        :items="$themeConfig.redesign.primary_right_nav"
+        :itemCss="'link link--small link--semi-bold'"
+        :subItemCss="'link link--small link--semi-bold link--black'"
+      />
+    </div>
+    <a class="sign-up--button" href="/signup/">Sign Up</a>
+  </header>
+</template>
+
+<script>
+export default {
+  components: {
+    MenuItems: () => import("../components/MenuItems.vue"),
+    MenuItem: () => import("../components/MenuItem.vue")
+  }
+};
+</script>
